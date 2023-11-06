622bd1a4135e4ada522205df793a63d23c54f2f0
Rehabescapi <jasonmlehmann@gmail.com>
2023-11-06 17:05:50 -0600

    whoops converting to records format

--- app/server.py
+++ app/server.py
@@ -71,4 +71,4 @@ def queryTest():
     print(" and " + str(sample))
     testdf = df[df['ID'] == 609772]
     print(testdf)
-    return testdf.json(orient='table')
+    return testdf.json(orient='records')
